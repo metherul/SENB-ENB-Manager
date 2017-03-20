46eaca761ab6934657605d2cd49fc46de064b568
metherul <greenmarleyethan@gmail.com>
2017-03-20 16:29:23 -0400

    Signed-off-by: metherul <greenmarleyethan@gmail.com>

---
+++ SENB ENB Manager/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SENB_ENB_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorZone_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void MinimizeWindow_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    }
+}
---
+++ SENB ENB Manager/TransitionHandler.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Command;
+using PropertyChanged;
+using System;
+
+namespace SENB_ENB_Manager
+{
+    [ImplementPropertyChanged]
+    class TransitionHandler
+    {
+        // Sets the current focused UserControl
+        public int CurrentViewIndex { get; set; }
+
+        // Handle buttons to change view in XAML
+        // object handles the current view.
+        public RelayCommand<object> ChangeViewCommand { get; set; }
+
+        public TransitionHandler()
+        {
+            ChangeViewCommand = new RelayCommand<object>(SetCurrentView);
+        }
+
+        public void SetCurrentView(object parameter)
+        {
+            CurrentViewIndex = Convert.ToInt32(parameter);
+        }
+    }
+}
